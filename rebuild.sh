8fe3210a36a0bea04c141e8aa07ba7ed51d79df8
Gabriel Minton <gminton07@gmail.com>
2025-04-05 23:07:55 -0500

    Package building script

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# I place this file in my home directory, so I can easily access it with
+# $ ~/rebuild.sh
+# Before using, you must add executable permissions through
+# $ chmod +x rebuild.sh
 
 echo "Hello cruel world!"
 echo ""
