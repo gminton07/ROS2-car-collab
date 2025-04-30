60855a75a6a5e1a884d96426ebb37c005757e22d
Gabriel Minton <gminton07@gmail.com>
2025-04-30 14:24:24 -0500

    4/30

--- pi_car/sub_LED.py
+++ pi_car/sub_LED.py
@@ -13,14 +13,18 @@ class RGB_ctrl(Node, Thread):
     left_R, left_G, left_B = (22, 23, 24)
     right_R, right_G, right_B = (10, 9, 25)
 
-    pins = None
-    pwmR, pwmG, pwmB = (None, None, None)
+    Lpins, Rpins = (None, None)
+    L_pwmR, L_pwmG, L_pwmB = (None, None, None)
+    R_pwmR, R_pwmG, R_pwmB = (None, None, None)
+
 
     def __init__(self, Node, Thread):
         super().__init__('rgb_ctrl')
         Thread.__init__(self)
 
         # Set up LEDs
+        self.setupLED(self.left_R, self.left_G, self.left_B,
+                self.right_R, self.right_G, self.right_B)
 
         self.subscription = self.create_subscription(
                 Float32,
@@ -48,36 +52,56 @@ class RGB_ctrl(Node, Thread):
         intersect = str(msg.data).split()
         intersect = bool(intersect)
 
+        if intersect:   self.start()
+
     def listener_red(self, msg):
         red = str(msg.data).split()
         red = bool(red)
 
         if red:
-            self.setColor(0xff0000)
+            self.setColor(0xff0000, 'B')
         self.get_logger().info('brake lights')
 
     # LED Control functions
-    def setupLED(self, Rpin, Gpin, Bpin):
-        self.pins = {'pin_R':Rpin, 'pin_G':Gpin, 'pin_B':Bpin}
+    def setupLED(self, L_Rpin, L_Gpin, L_Bpin, R_Rpin, R_Gpin, R_Bpin):
+        self.Lpins = {'pin_R':L_Rpin, 'pin_G':L_Gpin, 'pin_B':L_Bpin}
+        self.Rpins = {'pin_R':R_Rpin, 'pin_G':R_Gpin, 'pin_B':R_Bpin}
 
-        for i in self.pins:
-            GPIO.setup(self.pins[i], GPIO.OUT)
-            GPIO.output(self.pins[i], GPIO.LOW)
 
-        self.pwmR = GPIO.PWM(self.pins['pin_R'], 50)
-        self.pwmG = GPIO.PWM(self.pins['pin_G'], 50)
-        self.pwmB = GPIO.PWM(self.pins['pin_B'], 50)
 
-        self.pwmR.start(0)
-        self.pwmG.start(0)
-        self.pwmB.start(0)
+        for i in self.Lpins:
+            GPIO.setup(self.Lpins[i], GPIO.OUT)
+            GPIO.output(self.Lpins[i], GPIO.HIGH)    # HIGH == off
+        for i in self.Rpins:
+            GPIO.setup(self.Rpins[i], GPIO.OUT)
+            GPIO.output(self.Rpins[i], GPIO.HIGH)    # HIGH == off
 
-        self.setColor(0x000000)
+        # Initialize left LED
+        self.L_pwmR = GPIO.PWM(self.Lpins['pin_R'], 5000)
+        self.L_pwmG = GPIO.PWM(self.Lpins['pin_G'], 5000)
+        self.L_pwmB = GPIO.PWM(self.Lpins['pin_B'], 5000)
+        self.L_pwmR.start(0)
+        self.L_pwmG.start(0)
+        self.L_pwmB.start(0)
+
+        # Initialize right LED
+        self.R_pwmR = GPIO.PWM(self.Rpins['pin_R'], 5000)
+        self.R_pwmG = GPIO.PWM(self.Rpins['pin_G'], 5000)
+        self.R_pwmB = GPIO.PWM(self.Rpins['pin_B'], 5000)
+        self.R_pwmR.start(0)
+        self.R_pwmG.start(0)
+        self.R_pwmB.start(0)
+
+        self.setColor(0x000000, 'L')
+        self.setColor(0x000000, 'R')
 
     def map(self, x, in_min, in_max, out_min, out_max):
         return 100 - (x-in_min) / (in_max-in_min) * (out_max-out_min) + out_min
 
-    def setColor(self, col):
+    def setColor(self, col, side):
+        # side chooses which LED is changed
+        # 'L' left, 'R' right, 'B' both
+
         R_val = (col & 0xff0000) >> 16
         G_val = (col & 0x00ff00) >> 8
         B_val = (col & 0x0000ff) >> 0
@@ -85,11 +109,16 @@ class RGB_ctrl(Node, Thread):
         R_val = self.map(R_val, 0, 255, 0, 100)
         G_val = self.map(G_val, 0, 255, 0, 100)
         B_val = self.map(B_val, 0, 255, 0, 100)
-
-        self.pwmR.ChangeDutyCycle(R_val)
-        self.pwmG.ChangeDutyCycle(G_val)
-        self.pwmB.ChangeDutyCycle(B_val)
-
+        
+        if side == 'L' or 'B':
+            self.L_pwmR.ChangeDutyCycle(R_val)
+            self.L_pwmG.ChangeDutyCycle(G_val)
+            self.L_pwmB.ChangeDutyCycle(B_val)
+        elif side == 'R' or 'B':
+            self.R_pwmR.ChangeDutyCycle(R_val)
+            self.R_pwmG.ChangeDutyCycle(G_val)
+            self.R_pwmB.ChangeDutyCycle(B_val)
+    
     def blink(self, col, tim):
         start_time = time.time()
         cur_time = start_time
@@ -101,23 +130,21 @@ class RGB_ctrl(Node, Thread):
 
             if (start_time + i * delay < cur_time):
                 if i%2 == 0:
-                    self.setColor(0x000000)
-                    #self.destroy()
+                    self.setColor(0x000000, 'B')    #TODO: Change with directions later
                 else:
-                    self.setColor(col)
+                    self.setColor(col, 'B')
                 
                 i += 1
 
     def off(self):
         for i in self.pins:
-            GPIO.output(self.pins[i], GPIO.LOW)
+            GPIO.output(self.pins[i], GPIO.LOW)    # Tied high is off
 
     def destroy(self):
         self.pwmR.stop()
         self.pwmG.stop()
         self.pwmB.stop()
-        for i in self.pins:
-            GPIO.output(self.pins[i], GPIO.LOW)
+        self.off()
 
     def run(self):
         self.blink(0xffff00, 5)
@@ -125,21 +152,15 @@ class RGB_ctrl(Node, Thread):
 
 def main(args=None):
     rclpy.init(args=args)
-    rgb_l = RGB_ctrl(Node, Thread)
-    rgb_l.setupLED(RGB_ctrl.left_R, RGB_ctrl.left_G, RGB_ctrl.left_B)
-    rgb_r = RGB_ctrl(Node, Thread)
-    rgb_r.setupLED(RGB_ctrl.right_R, RGB_ctrl.right_G, RGB_ctrl.right_B)
+    rgb_ctrl = RGB_ctrl(Node, Thread)
 
-    rgb_l.start()
-    rgb_r.blink(0xffff00, 5)
+    #rgb_ctrl.off()
 
-    rclpy.spin(rgb_l)
-    rclpy.spin(rgb_r)
+    rclpy.spin(rgb_ctrl)
 
-    rgb_l.destroy()
-    rgb_r.destroy()
-    rgb_l.destroy_node()
-    rgb_r.destroy_node()
+    rgb_ctrl.destroy()
+    GPIO.cleanup()
+    rgb_ctrl.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
